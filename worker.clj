d1788c21b501099b32e8760dc74976a3b2e54627
Tobias Burger <tobias_burger@hotmail.com>
2023-07-20 11:23:57 +0200

    Reformatted code

--- worker.clj
+++ worker.clj
@@ -4,11 +4,11 @@
 (defn dostuff []
   (do
     (print "I'm doing lazy work\n")
-  		(print "Resting for 10 seconds\n")
+    (print "Resting for 10 seconds\n")
     (flush)))
 
-		(loop [x 1]
-  		(when (< x 10)
-  				(dostuff)
-  				(Thread/sleep 10000)
-    		(recur (+ x 1))))
+(loop [x 1]
+  (when (< x 10)
+    (dostuff)
+    (Thread/sleep 10000)
+    (recur (+ x 1))))
