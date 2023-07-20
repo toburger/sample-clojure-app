2a10b59b9daa7281b476f0161a993b0f0a1cdd55
Tobias Burger <tobias_burger@hotmail.com>
2023-07-20 13:18:57 +0200

    Remove babashka

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 jwsgi: java -jar target/sample-clojure-app-0.1.0-SNAPSHOT.jar
-#worker: bb worker.clj
-#cron: */1 * * * * bb hello.clj
--- install-prerequisites.sh
+++
@@ -1,4 +0,0 @@
-curl -o install-bb -sLO https://raw.githubusercontent.com/babashka/babashka/master/install
-chmod +x install-bb
-./install-bb --dir ./bin/
-rm install-bb
